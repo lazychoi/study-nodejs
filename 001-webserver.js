27a65fd18267797c4aa677c75a0aaeb8a9b12812
jychoi <eduroad@gmail.com>
2018-08-10 14:07:22 +0900

    웹서버에 호스트, 백로그(실질적으로 동시 접속 연결수를 결정하는 정보) 추가

--- 001-webserver.js
+++ 001-webserver.js
@@ -4,7 +4,8 @@ var http = require('http');
 var server = http.createServer();
 
 // 웹 서버 시작하고 3000번 포트에서 대기함
+var host = 'localhost';
 var port = 3000;
-server.listen(3000, function(){
-    console.log('웹 서버가 %d 포트에서 시작되었습니다.', port);
+server.listen(port, host, 50000, function(){
+    console.log('웹 서버가 http://%s:%d에서 시작되었습니다.', host, port);
 })
